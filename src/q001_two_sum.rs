739af9a71feaef6ec593300944b0f01da3b64cd2
wlynxg <liuauthor@foxmail.com>
2024-06-27 21:02:30 +0800

    feat: optimization q001_two_sum

--- src/q001_two_sum.rs
+++ src/q001_two_sum.rs
@@ -52,12 +52,12 @@ pub struct Solution {}
 //leetcode submit region begin(Prohibit modification and deletion)
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut map: HashMap<i32, i32> = HashMap::with_capacity(nums.len());
+        let mut map: HashMap<i32, usize> = HashMap::with_capacity(nums.len());
         for (i, num) in nums.iter().enumerate() {
             if let Some(&j) = map.get(&(target - num)) {
-                return vec![j, i as i32]
+                return vec![j as i32, i as i32]
             }
-            map.insert(*num, i as i32);
+            map.insert(*num, i);
         }
         vec![]
     }

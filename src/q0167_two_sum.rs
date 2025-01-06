e34851733d4be8e570d4d78cc7259464b86fc037
wlynxg <liuauthor@foxmail.com>
2025-01-06 17:56:32 +0800

    feat: complete "[167]Two Sum II - Input Array Is Sorted"

---
+++ src/q0167_two_sum.rs
@@ -0,0 +1,85 @@
+//Given a 1-indexed array of integers numbers that is already sorted in non-
+//decreasing order, find two numbers such that they add up to a specific target
+//number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1
+// < index2 <= numbers.length.
+//
+// Return the indices of the two numbers, index1 and index2, added by one as an
+//integer array [index1, index2] of length 2.
+//
+// The tests are generated such that there is exactly one solution. You may not
+//use the same element twice.
+//
+// Your solution must use only constant extra space.
+//
+//
+// Example 1:
+//
+//
+//Input: numbers = [2,7,11,15], target = 9
+//Output: [1,2]
+//Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We
+//return [1, 2].
+//
+//
+// Example 2:
+//
+//
+//Input: numbers = [2,3,4], target = 6
+//Output: [1,3]
+//Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We
+//return [1, 3].
+//
+//
+// Example 3:
+//
+//
+//Input: numbers = [-1,0], target = -1
+//Output: [1,2]
+//Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We
+//return [1, 2].
+//
+//
+//
+// Constraints:
+//
+//
+// 2 <= numbers.length <= 3 * 10⁴
+// -1000 <= numbers[i] <= 1000
+// numbers is sorted in non-decreasing order.
+// -1000 <= target <= 1000
+// The tests are generated such that there is exactly one solution.
+//
+//
+// Related Topics Array Two Pointers Binary Search 👍 12167 👎 1448
+
+pub struct Solution;
+//leetcode submit region begin(Prohibit modification and deletion)
+impl Solution {
+    pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut left = 0;
+        let mut right = numbers.len() - 1;
+        while left < right {
+            let sum = numbers[left] + numbers[right];
+            if sum == target {
+                return vec![left as i32 + 1, right as i32 + 1];
+            } else if sum > target {
+                right -= 1;
+            } else {
+                left += 1;
+            }
+        }
+        vec![]
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+#[cfg(test)]
+mod tests {
+    use crate::q0167_two_sum::Solution;
+
+    #[test]
+    fn test_two_sum() {
+        assert_eq!(vec![1, 2], Solution::two_sum(vec![2, 7, 11, 15], 9));
+        assert_eq!(vec![1, 3], Solution::two_sum(vec![2, 3, 4], 6));
+        assert_eq!(vec![1, 2], Solution::two_sum(vec![-1, 0], -1));
+    }
+}

6c976e47ee1138abe85cca330e21bd9b281e5473
wlynxg <liuauthor@foxmail.com>
2025-01-18 16:13:58 +0800

    feat: complete "[456]132 Pattern"

--- src/main.rs
+++ src/main.rs
@@ -58,6 +58,7 @@ mod q0389_find_the_difference;
 mod q0414_third_max;
 mod q0438_find_anagrams;
 mod q0448_find_disappeared_numbers;
+mod q0456_find132pattern;
 mod q0459_repeated_substring_pattern;
 mod q0476_find_complement;
 mod q0482_license_key_formatting;
---
+++ src/q0456_find132pattern.rs
@@ -0,0 +1,79 @@
+//Given an array of n integers nums, a 132 pattern is a subsequence of three
+//integers nums[i], nums[j] and nums[k] such that i < j < k and nums[i] < nums[k] <
+//nums[j].
+//
+// Return true if there is a 132 pattern in nums, otherwise, return false.
+//
+//
+// Example 1:
+//
+//
+//Input: nums = [1,2,3,4]
+//Output: false
+//Explanation: There is no 132 pattern in the sequence.
+//
+//
+// Example 2:
+//
+//
+//Input: nums = [3,1,4,2]
+//Output: true
+//Explanation: There is a 132 pattern in the sequence: [1, 4, 2].
+//
+//
+// Example 3:
+//
+//
+//Input: nums = [-1,3,2,0]
+//Output: true
+//Explanation: There are three 132 patterns in the sequence: [-1, 3, 2], [-1, 3,
+// 0] and [-1, 2, 0].
+//
+//
+//
+// Constraints:
+//
+//
+// n == nums.length
+// 1 <= n <= 2 * 10⁵
+// -10⁹ <= nums[i] <= 10⁹
+//
+//
+// Related Topics Array Binary Search Stack Monotonic Stack Ordered Set 👍 7337
+//👎 443
+
+pub struct Solution;
+//leetcode submit region begin(Prohibit modification and deletion)
+impl Solution {
+    pub fn find132pattern(nums: Vec<i32>) -> bool {
+        let mut stack = vec![];
+        let mut numk = i32::MIN;
+
+        for i in (0..nums.len()).rev() {
+            if nums[i] < numk {
+                return true;
+            }
+            while !stack.is_empty() && stack[stack.len() - 1] < nums[i] {
+                if let Some(j) = stack.pop() {
+                    numk = numk.max(j);
+                }
+            }
+            stack.push(nums[i]);
+        }
+
+        false
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_find132pattern() {
+        assert_eq!(Solution::find132pattern(vec![1, 2, 3, 4]), false);
+        assert_eq!(Solution::find132pattern(vec![3, 1, 4, 2]), true);
+        assert_eq!(Solution::find132pattern(vec![-1, 3, 2, 0]), true);
+        assert_eq!(Solution::find132pattern(vec![1, 0, 1, -4, -3]), false);
+        assert_eq!(Solution::find132pattern(vec![3, 5, 0, 3, 4]), true);
+    }
+}
